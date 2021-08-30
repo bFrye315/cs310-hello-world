ffc869aaf99a7ac2674df00299bb44e8506088be
Brandon <bhf315@gmail.com>
2021-08-30 11:19:19 -0500

    Fixed a mistake I made

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Class1 c1 = new Class1();
-        Class3 c2 = new Class2();
+        Class2 c2 = new Class2();
         System.out.println("Hello, World!");
     }
     private void f1() {
